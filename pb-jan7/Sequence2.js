7e2e9ce831e1d3f211734362e8d294c92e6fa01c
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-29 23:19:40 +0300

    Add files via upload

---
+++ pb-jan7/Sequence2.js
@@ -0,0 +1,13 @@
+function sequence(input) {
+
+    let n = Number(input[0])
+
+    let k = 1 
+    while (n >= k) {
+
+        console.log(k)
+        k = k * 2 + 1
+        n++
+    }
+}
+sequence(["8"])
