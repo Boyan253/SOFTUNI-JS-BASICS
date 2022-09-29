db233e27b5890679f9dfd367d95096b3f1ccce50
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-29 23:19:28 +0300

    Add files via upload

---
+++ pb-jan7/MinNumber.js
@@ -0,0 +1,25 @@
+function maxnumber(input) {
+    let index = 0;
+    let num = input[index]
+    index++;
+    let maxNumber = Number.MIN_SAFE_INTEGER
+    
+    while(num !== "Stop"){
+    let command = Number(num);
+    if (maxNumber < command) {
+    maxNumber = num;
+    
+    }
+    
+    num = input[index]
+    index++
+    
+    }
+    console.log(maxNumber)
+    
+    } maxnumber(["-10",
+    "20",
+    "-30",
+    "Stop"])
+    
+    
