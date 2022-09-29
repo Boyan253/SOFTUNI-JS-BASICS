4932157c116eb73b01105646dc1890b977db0645
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-29 23:15:29 +0300

    Add files via upload

---
+++ pb-jan7/accountBalance.js
@@ -0,0 +1,29 @@
+function moneysmetka(input) {
+let index = 0;
+let command = input[index]
+
+let account = 0
+index ++
+
+
+
+while(command !== "NoMoreMoney" ){
+
+let money = Number(command)
+
+
+if (money < 0) {
+    console.log("Invalid operation!" )
+    break;
+}
+ account += money
+ console.log("Increase: " + money.toFixed(2))
+
+command = input[index]
+index ++
+}
+console.log("Total: " + account.toFixed(2))
+}moneysmetka(["5.51", 
+"69.42",
+"100",
+"NoMoreMoney"])
