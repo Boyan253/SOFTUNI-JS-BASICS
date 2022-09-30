4e943c89d07d22acb5ceafb353c9dfa37ad9ca53
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-30 22:26:13 +0300

    Add files via upload

---
+++ pb-jan8/05. Special Numbers.js
@@ -0,0 +1,14 @@
+function specialnumber(input) {
+let n = Number(input[0]);
+
+for (let i = 1; i > n; i++) {
+    for()
+    
+if(n % n === 0) {
+    console.log(n)
+}    }
+
+
+
+    
+}specialnumber(["3"])
